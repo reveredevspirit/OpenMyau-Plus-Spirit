33c10fc3c05d095dbe12e345875d0ed8a8b3c800
spirit <spirit.business15@gmail.com>
2026-02-19 00:38:08 +1100

    Update Velocity.java

--- src/main/java/myau/module/modules/Velocity.java
+++ src/main/java/myau/module/modules/Velocity.java
@@ -28,12 +28,12 @@ public class Velocity extends Module {
     }
 
     private boolean canDelay() {
-        KillAura killAura = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
-        Autoblock autoblock = (Autoblock) Myau.moduleManager.modules.get(Autoblock.class);
+    KillAura killAura = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
+    Autoblock autoblock = (Autoblock) Myau.moduleManager.modules.get(Autoblock.class);
 
-        return mc.thePlayer.onGround
-                && (!killAura.isEnabled() || !autoblock.isBlocking());
-    }
+    return mc.thePlayer.onGround
+            && (!killAura.isEnabled() || !autoblock.isBlocking());
+}
 
     @EventTarget
     public void onKnockback(KnockbackEvent event) {
