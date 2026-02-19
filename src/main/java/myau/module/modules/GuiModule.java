ef2a06e8c490ab77450fba94657182af84d3c07d
spirit <spirit.business15@gmail.com>
2026-02-19 18:55:59 +1100

    Update GuiModule.java

--- src/main/java/myau/module/modules/GuiModule.java
+++ src/main/java/myau/module/modules/GuiModule.java
@@ -1,41 +1,16 @@
-package myau.module.modules;
+@Override
+public void onEnabled() {
+    setEnabled(false);
 
-import myau.module.Module;
-import myau.ui.ClickGui;
-import myau.ui.clickgui.Rise6ClickGui;
-import net.minecraft.client.Minecraft;
-import org.lwjgl.input.Keyboard;
+    // DO NOT INITIALIZE OLD CLICKGUI ANYMORE
 
-public class GuiModule extends Module {
+    clickGui = new Rise6ClickGui(
+            ClickGui.combatModules,
+            ClickGui.movementModules,
+            ClickGui.playerModules,
+            ClickGui.renderModules,
+            ClickGui.miscModules
+    );
 
-    private static final Minecraft mc = Minecraft.getMinecraft();
-    private Rise6ClickGui clickGui;
-
-    public GuiModule() {
-        super("ClickGui", false);
-        setKey(Keyboard.KEY_RSHIFT);
-    }
-
-    @Override
-    public void onEnabled() {
-        // Disable the module immediately (GUI modules should not stay "enabled")
-        setEnabled(false);
-
-        // Ensure ClickGui is initialized so module lists are populated
-        if (ClickGui.combatModules == null) {
-            new ClickGui();
-        }
-
-        // Create Rise6ClickGui instance
-        clickGui = new Rise6ClickGui(
-                ClickGui.combatModules,
-                ClickGui.movementModules,
-                ClickGui.playerModules,
-                ClickGui.renderModules,
-                ClickGui.miscModules
-        );
-
-        // Open the GUI
-        mc.displayGuiScreen(clickGui);
-    }
+    mc.displayGuiScreen(clickGui);
 }
