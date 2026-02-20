babfe9331b7d957a040979250f519666738f6bf5
spirit <spirit.business15@gmail.com>
2026-02-20 22:18:13 +1100

    Refactor AutoAnduril to use new settings structure

--- src/main/java/myau/module/modules/AutoAnduril.java
+++ src/main/java/myau/module/modules/AutoAnduril.java
@@ -5,9 +5,9 @@ import myau.event.EventTarget;
 import myau.event.types.EventType;
 import myau.event.types.Priority;
 import myau.events.TickEvent;
+import myau.module.BooleanSetting;
 import myau.module.Module;
-import myau.property.properties.BooleanProperty;
-import myau.property.properties.IntProperty;
+import myau.module.SliderSetting;
 import myau.util.ItemUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemBlock;
@@ -20,75 +20,79 @@ import net.minecraft.util.MovingObjectPosition;
 public class AutoAnduril extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
     private int previousSlot = -1;
-    private int currentSlot = -1;
+    private int currentSlot  = -1;
     private int intervalTick = -1;
-    private int holdTick = -1;
-    public final IntProperty interval = new IntProperty("interval", 40, 0, 100);
-    public final IntProperty hold = new IntProperty("hold", 1, 0, 20);
-    public final BooleanProperty speedCheck = new BooleanProperty("speed-check", false);
-    public final IntProperty debug = new IntProperty("debug", 0, 0, 9);
+    private int holdTick     = -1;
+
+    public final SliderSetting  interval   = new SliderSetting("Interval",   40, 0, 100, 1);
+    public final SliderSetting  hold       = new SliderSetting("Hold",        1, 0,  20, 1);
+    public final BooleanSetting speedCheck = new BooleanSetting("Speed Check", false);
+    public final SliderSetting  debug      = new SliderSetting("Debug",        0, 0,   9, 1);
 
     public AutoAnduril() {
         super("AutoAnduril", false);
+        register(interval);
+        register(hold);
+        register(speedCheck);
+        register(debug);
     }
 
     public boolean canSwap() {
         if (mc.objectMouseOver != null
                 && mc.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK
                 && mc.gameSettings.keyBindAttack.isKeyDown()) return false;
-        ItemStack currentItem = mc.thePlayer.inventory.getStackInSlot(mc.thePlayer.inventory.currentItem);
-        if (currentItem != null) {
-            if (currentItem.getItem() instanceof ItemBlock && mc.gameSettings.keyBindUseItem.isKeyDown()) return false;
-            if (!(currentItem.getItem() instanceof ItemSword) && mc.thePlayer.isUsingItem()) return false;
+        ItemStack cur = mc.thePlayer.inventory.getStackInSlot(mc.thePlayer.inventory.currentItem);
+        if (cur != null) {
+            if (cur.getItem() instanceof ItemBlock && mc.gameSettings.keyBindUseItem.isKeyDown()) return false;
+            if (!(cur.getItem() instanceof ItemSword) && mc.thePlayer.isUsingItem()) return false;
         }
         InvWalk invWalk = (InvWalk) Myau.moduleManager.modules.get(InvWalk.class);
-        return mc.currentScreen == null || mc.currentScreen instanceof myau.ui.clickgui.Rise6ClickGui
+        return mc.currentScreen == null
+                || mc.currentScreen instanceof myau.ui.clickgui.Rise6ClickGui
                 || invWalk.isEnabled() && invWalk.canInvWalk();
     }
 
     public boolean hasSpeed() {
         if (!speedCheck.getValue()) return false;
-        PotionEffect potionEffect = mc.thePlayer.getActivePotionEffect(Potion.moveSpeed);
-        if (potionEffect == null) return false;
-        return (potionEffect.getAmplifier() > 0);
+        PotionEffect pe = mc.thePlayer.getActivePotionEffect(Potion.moveSpeed);
+        return pe != null && pe.getAmplifier() > 0;
     }
 
     @EventTarget(Priority.LOWEST)
     public void onTick(TickEvent event) {
-        if (this.isEnabled() && event.getType() == EventType.PRE) {
-            if (this.currentSlot != -1 && this.currentSlot != mc.thePlayer.inventory.currentItem) {
-                this.currentSlot = -1;
-                this.previousSlot = -1;
-                this.intervalTick = interval.getValue();
-                this.holdTick = -1;
+        if (isEnabled() && event.getType() == EventType.PRE) {
+            if (currentSlot != -1 && currentSlot != mc.thePlayer.inventory.currentItem) {
+                currentSlot = -1;
+                previousSlot = -1;
+                intervalTick = (int) interval.getValue();
+                holdTick = -1;
             }
-
-            if (this.intervalTick > 0) {
-                this.intervalTick--;
+            if (intervalTick > 0) {
+                intervalTick--;
             } else if (intervalTick == 0) {
                 if (canSwap() && !hasSpeed()) {
                     int slot = ItemUtil.findAndurilHotbarSlot(mc.thePlayer.inventory.currentItem);
-                    if (debug.getValue() != 0 && slot == -1) slot = debug.getValue() - 1;
+                    if ((int) debug.getValue() != 0 && slot == -1) slot = (int) debug.getValue() - 1;
                     if (slot != -1 && slot != mc.thePlayer.inventory.currentItem) {
-                        this.previousSlot = mc.thePlayer.inventory.currentItem;
-                        this.currentSlot = mc.thePlayer.inventory.currentItem = slot;
-                        this.intervalTick = -1;
-                        this.holdTick = hold.getValue();
+                        previousSlot = mc.thePlayer.inventory.currentItem;
+                        currentSlot  = mc.thePlayer.inventory.currentItem = slot;
+                        intervalTick = -1;
+                        holdTick     = (int) hold.getValue();
                         return;
                     } else {
-                        this.intervalTick = interval.getValue();
-                        this.holdTick = -1;
+                        intervalTick = (int) interval.getValue();
+                        holdTick = -1;
                     }
                 }
             }
-            if (this.holdTick > 0) {
-                this.holdTick--;
+            if (holdTick > 0) {
+                holdTick--;
             } else if (holdTick == 0) {
-                if (this.previousSlot != -1 && canSwap()) {
-                    mc.thePlayer.inventory.currentItem = this.previousSlot;
-                    this.previousSlot = -1;
-                    this.holdTick = -1;
-                    this.intervalTick = interval.getValue();
+                if (previousSlot != -1 && canSwap()) {
+                    mc.thePlayer.inventory.currentItem = previousSlot;
+                    previousSlot = -1;
+                    holdTick = -1;
+                    intervalTick = (int) interval.getValue();
                 }
             }
         }
@@ -96,17 +100,17 @@ public class AutoAnduril extends Module {
 
     @Override
     public void onEnabled() {
-        this.previousSlot = -1;
-        this.currentSlot = -1;
-        this.intervalTick = this.interval.getValue();
-        this.holdTick = -1;
+        previousSlot = -1;
+        currentSlot  = -1;
+        intervalTick = (int) interval.getValue();
+        holdTick     = -1;
     }
 
     @Override
     public void onDisabled() {
-        this.previousSlot = -1;
-        this.currentSlot = -1;
-        this.intervalTick = -1;
-        this.holdTick = -1;
+        previousSlot = -1;
+        currentSlot  = -1;
+        intervalTick = -1;
+        holdTick     = -1;
     }
 }
