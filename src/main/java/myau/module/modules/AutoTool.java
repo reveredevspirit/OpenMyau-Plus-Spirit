f62acc000285aae6f0ea779564e51bcf2cd5de3e
spirit <spirit.business15@gmail.com>
2026-02-20 22:17:11 +1100

    Update AutoTool.java

--- src/main/java/myau/module/modules/AutoTool.java
+++ src/main/java/myau/module/modules/AutoTool.java
@@ -4,26 +4,30 @@ import myau.Myau;
 import myau.event.EventTarget;
 import myau.event.types.EventType;
 import myau.events.TickEvent;
+import myau.module.BooleanSetting;
 import myau.module.Module;
+import myau.module.SliderSetting;
 import myau.util.ItemUtil;
 import myau.util.KeyBindUtil;
-import myau.property.properties.BooleanProperty;
-import myau.property.properties.IntProperty;
 import myau.util.TeamUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.MovingObjectPosition.MovingObjectType;
 
 public class AutoTool extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
-    private int currentToolSlot = -1;
-    private int previousSlot = -1;
+    private int currentToolSlot  = -1;
+    private int previousSlot     = -1;
     private int tickDelayCounter = 0;
-    public final IntProperty switchDelay = new IntProperty("delay", 0, 0, 5);
-    public final BooleanProperty switchBack = new BooleanProperty("switch-back", true);
-    public final BooleanProperty sneakOnly = new BooleanProperty("sneak-only", true);
+
+    public final SliderSetting  switchDelay = new SliderSetting("Delay", 0, 0, 5, 1);
+    public final BooleanSetting switchBack  = new BooleanSetting("Switch Back", true);
+    public final BooleanSetting sneakOnly   = new BooleanSetting("Sneak Only", true);
 
     public AutoTool() {
         super("AutoTool", false);
+        register(switchDelay);
+        register(switchBack);
+        register(sneakOnly);
     }
 
     public boolean isKillAura() {
@@ -37,32 +41,29 @@ public class AutoTool extends Module {
         if (this.isEnabled() && event.getType() == EventType.PRE) {
             if (this.currentToolSlot != -1 && this.currentToolSlot != mc.thePlayer.inventory.currentItem) {
                 this.currentToolSlot = -1;
-                this.previousSlot = -1;
+                this.previousSlot    = -1;
             }
             if (mc.objectMouseOver != null
                     && mc.objectMouseOver.typeOfHit == MovingObjectType.BLOCK
                     && mc.gameSettings.keyBindAttack.isKeyDown()
                     && !mc.thePlayer.isUsingItem()
                     && !isKillAura()) {
-                if (this.tickDelayCounter >= this.switchDelay.getValue()
-                        && (!(Boolean) this.sneakOnly.getValue() || KeyBindUtil.isKeyDown(mc.gameSettings.keyBindSneak.getKeyCode()))) {
+                if (this.tickDelayCounter >= (int) this.switchDelay.getValue()
+                        && (!this.sneakOnly.getValue() || KeyBindUtil.isKeyDown(mc.gameSettings.keyBindSneak.getKeyCode()))) {
                     int slot = ItemUtil.findInventorySlot(
-                            mc.thePlayer.inventory.currentItem, mc.theWorld.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock()
-                    );
+                            mc.thePlayer.inventory.currentItem,
+                            mc.theWorld.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock());
                     if (mc.thePlayer.inventory.currentItem != slot) {
-                        if (this.previousSlot == -1) {
-                            this.previousSlot = mc.thePlayer.inventory.currentItem;
-                        }
+                        if (this.previousSlot == -1) this.previousSlot = mc.thePlayer.inventory.currentItem;
                         mc.thePlayer.inventory.currentItem = this.currentToolSlot = slot;
                     }
                 }
                 this.tickDelayCounter++;
             } else {
-                if (this.switchBack.getValue() && this.previousSlot != -1) {
+                if (this.switchBack.getValue() && this.previousSlot != -1)
                     mc.thePlayer.inventory.currentItem = this.previousSlot;
-                }
-                this.currentToolSlot = -1;
-                this.previousSlot = -1;
+                this.currentToolSlot  = -1;
+                this.previousSlot     = -1;
                 this.tickDelayCounter = 0;
             }
         }
@@ -70,8 +71,8 @@ public class AutoTool extends Module {
 
     @Override
     public void onDisabled() {
-        this.currentToolSlot = -1;
-        this.previousSlot = -1;
+        this.currentToolSlot  = -1;
+        this.previousSlot     = -1;
         this.tickDelayCounter = 0;
     }
 }
